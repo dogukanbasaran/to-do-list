3aae1a73f62e02c5dbd8da8aaff6742d370e1dcf
Dogukan Basaran <dogukanbasarann@hotmail.com>
2024-09-18 22:29:59 +0300

    function was added: adding a new  task

---
+++ main.js
@@ -0,0 +1,19 @@
+const taskInput = document.querySelector(".task-input input");
+const addButton = document.getElementById("add-btn");
+const tasks = document.getElementById("tasks");
+const emptySpan = document.querySelector(".task-list ul > span");
+
+
+
+addButton.addEventListener("click", () => {
+   if(taskInput.value != ""){
+    let newListItem = document.createElement("li");
+    newListItem.innerHTML = `${taskInput.value} <button><i class="fa-solid fa-ellipsis-vertical"></i></button>`;
+    tasks.appendChild(newListItem);
+   }
+   else{
+    alert("you have to add at least 1 task.")
+   }
+
+   tasks.removeChild(emptySpan);
+});
