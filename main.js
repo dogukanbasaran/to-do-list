d33fc0de77d4bdbb934aae3d69f5b15445b1bd82
Dogukan Basaran <dogukanbasarann@hotmail.com>
2024-09-19 16:55:32 +0300

    fixing: cleaning input and removing span element

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ addButton.addEventListener("click", () => {
     let newListItem = document.createElement("li");
     newListItem.innerHTML = `${taskInput.value} <button><i class="fa-solid fa-ellipsis-vertical"></i></button>`;
     tasks.appendChild(newListItem);
+    tasks.removeChild(emptySpan);
    }
    else{
     alert("you have to add at least 1 task.")
    }
-
-   tasks.removeChild(emptySpan);
 });
+
+addButton.addEventListener("click", () => {
+   taskInput.value = "";
+})
